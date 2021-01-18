f1fa23450bfb2041634b0dc286901dc681d7c9c1
Arturo <gregory09ruiz@gmail.com>
2021-01-17 22:15:49 -0400

    Multiples of 3 or 5

---
+++ Javascript/6 Kyu/Multiples of 3 or 5/app.js
@@ -0,0 +1,10 @@
+function solution(number) {
+    var s = 0;
+
+    for (var i = 1; i < number; i++) {
+        if (i % 3 == 0 || i % 5 == 0) {
+            s += i
+        }
+    }
+    return s;
+}
